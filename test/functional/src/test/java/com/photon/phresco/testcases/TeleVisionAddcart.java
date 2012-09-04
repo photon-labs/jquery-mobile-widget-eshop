7c51080edef1a8eecc824f85dc55c2b591d97053
srividya <srividya.subramani@photoninfotech.net>
2012-09-04 19:21:12 +0530

    "[artf527064]"Added Functional Folder

---
+++ test/functional/src/test/java/com/photon/phresco/testcases/TeleVisionAddcart.java
@@ -0,0 +1,70 @@
+package com.photon.phresco.testcases;
+
+import java.io.IOException;
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.openqa.selenium.server.SeleniumServer;
+
+import com.photon.phresco.Screens.MenuScreen;
+import com.photon.phresco.Screens.WelcomeScreen;
+import com.photon.phresco.uiconstants.WidgetData;
+import com.photon.phresco.uiconstants.PhrescoUiConstants;
+import com.photon.phresco.uiconstants.UIConstants;
+import com.thoughtworks.selenium.Selenium;
+
+public class TeleVisionAddcart extends TestCase {
+
+
+	private UIConstants phrsc;
+	private PhrescoUiConstants phr;
+	private WidgetData mobwigdata;
+	private WelcomeScreen wel;
+	private int SELENIUM_PORT;
+	private String browserAppends;
+	//private LoginScreen loginObject;
+	//private Log log = LogFactory.getLog(getClass());
+    String methodName;
+	@Test
+	public void testTV() throws InterruptedException, IOException, Exception {
+
+		try {
+
+			phr = new PhrescoUiConstants();
+			mobwigdata = new WidgetData();
+			String serverURL = phr.PROTOCOL + "://"
+					+ phr.HOST + ":"
+					+ phr.PORT + "/";
+			browserAppends = "*" + phr.BROWSER;
+			assertNotNull("Browser name should not be null",browserAppends);
+			SELENIUM_PORT = Integer.parseInt(phr.SERVER_PORT);
+			assertNotNull("selenium-port number should not be null",
+					SELENIUM_PORT);
+			wel=new WelcomeScreen(phr.SERVER_HOST, SELENIUM_PORT,
+					browserAppends, serverURL, phr.SPEED,
+					phr.CONTEXT );
+			assertNotNull(wel);
+			methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
+			System.out.println("methodName = " + methodName);
+			MenuScreen menu = wel.menuScreen(phrsc,methodName);
+			menu.Television(methodName);
+			menu.BillingInfo(mobwigdata,methodName);
+		} catch (Exception t) {
+			t.printStackTrace();
+			System.out.println("ScreenCaptured");
+			
+		}
+	}
+
+	public void setUp() throws Exception {
+		phrsc = new UIConstants();
+	}
+
+	public void tearDown() {
+		clean();
+	}
+
+	private void clean() {
+		wel.closeBrowser();
+	}
+
+}
