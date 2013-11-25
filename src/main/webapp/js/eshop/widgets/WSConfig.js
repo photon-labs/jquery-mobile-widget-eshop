cfc67cd290863868e377796a12c09e2f37876a4a
mohamedibrahim-s <mohamedibrahim.syedabuthahir@photoninfotech.net>
2013-11-25 16:58:34 +0530

    artf762006 , artf743310 bug fixing

--- src/main/webapp/js/eshop/widgets/WSConfig.js
+++ src/main/webapp/js/eshop/widgets/WSConfig.js
@@ -10,22 +10,17 @@ define( "eshop/widgets/WSConfig", [ "jquery" ], function($) {
 	WSConfig.prototype.context = undefined;
 	WSConfig.prototype.protocol = undefined;
 	WSConfig.prototype.WSConfigurl = {};
-	var serviceName = "eshopService";
 	
 	WSConfig.prototype.getEnvironment = function(callback) {
 		var self = this;
 		$.get("test/resources/phresco-env-config.xml",function(data){
 			$(data).find("environment").each(function() {
-			var env = $(this).attr('name');
 				$(this).find("WebService").each(function() {
-					var configServiceName = $(this).attr("name");
-					if (configServiceName === serviceName) {
-						self.WSConfigurl.host = $(this).find("host").text();
-						self.WSConfigurl.port = $(this).find("port").text();
-						self.WSConfigurl.context = $(this).find("context").text();
-						self.WSConfigurl.protocol = $(this).find("protocol").text();
-						callback(self.WSConfigurl); 
-					}
+					self.WSConfigurl.host = $(this).find("host").text();
+					self.WSConfigurl.port = $(this).find("port").text();
+					self.WSConfigurl.context = $(this).find("context").text();
+					self.WSConfigurl.protocol = $(this).find("protocol").text();
+					callback(self.WSConfigurl); 
 				});
 			});	
 		});
